587cb97e4e26071c00dc3ce4b6c3752d02b4f1a7
Noldor5150 <ruikis.paulius@gmail.com>
2019-04-08 21:26:59 +0300

    pabaigiau coupona, ir pradejom footertopa

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,7 +49,7 @@ module.exports = function(grunt) {
         cwd: 'web/assets/vendor/material-design-icons',
         src: [
           '../../../../web/assets/images/ic_menu_24px.svg',
-          '../../../../web/assets/images/ic_discount_24px.svg'
+          '../../../../web/assets/images/ic_calc_24px.svg'
         ],
         dest: 'src/sprites',
         options: {
